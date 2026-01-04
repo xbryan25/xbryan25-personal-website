abb560b5555d80d5ba54c1168875ca333701e4e2
Bryan Agan <bryanagan25@gmail.com>
2026-01-04 10:53:01 +0800

    feat(home-page): add animation in homepage using TailwindCSS and Inspira UI components

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -9,52 +9,83 @@ const isDark = computed({
     colorMode.preference = _isDark ? 'dark' : 'light';
   },
 });
+
+const show = ref(false);
+
+onMounted(() => {
+  // Trigger animation on mount
+  requestAnimationFrame(() => {
+    show.value = true;
+  });
+});
 </script>
 
 <template>
   <div class="flex-1 flex items-center justify-center h-full">
     <div class="flex flex-col items-center gap-8 mb-15">
-      <NuxtImg src="/images/self-portrait.jpg" class="rounded-full aspect-square w-32" />
+      <NuxtImg
+        src="/images/self-portrait.jpg"
+        class="rounded-full aspect-square w-32 opacity-0 scale-10 transition-all duration-2000 ease-out"
+        :class="show ? 'opacity-100 scale-100' : ''"
+      />
+
       <div class="flex gap-5 pr-16">
-        <p class="font-bold text-7xl">Bryan Agan</p>
+        <!-- <p class="font-bold text-7xl">Bryan Agan</p> -->
+        <SparklesText
+          text="Bryan Agan"
+          :colors="{ first: '#9E7AFF', second: '#FE8BBB' }"
+          :sparkles-count="10"
+          class="font-bold text-7xl"
+        />
         <USeparator orientation="vertical" class="h-20" color="primary" size="xl" />
-        <p class="font-bold text-7xl">xbryan25</p>
+        <SparklesText
+          text="xbryan25"
+          :colors="{ first: '#9E7AFF', second: '#FE8BBB' }"
+          :sparkles-count="10"
+          class="font-bold text-7xl"
+        />
       </div>
-      <p class="font-semibold text-xl">A full-stack web developer</p>
-      <div class="flex gap-10">
-        <a
-          href="https://www.linkedin.com/in/bryan-agan-42ba0b202/"
-          target="_blank"
-          class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
-        >
-          <NuxtImg
-            :src="isDark ? '/images/linkedin.png' : '/images/linkedin-dark.png'"
-            class="w-full h-full object-cover"
-            alt="Build image"
-          />
-        </a>
-        <a
-          href="https://github.com/xbryan25"
-          target="_blank"
-          class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
-        >
-          <NuxtImg
-            :src="isDark ? '/images/github.png' : '/images/github-dark.png'"
-            class="w-full h-full object-cover"
-            alt="Build image"
-          />
-        </a>
-        <a
-          href="mailto:bryanaganp25@gmail.com"
-          class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
-        >
-          <NuxtImg
-            :src="isDark ? '/images/email.png' : '/images/email-dark.png'"
-            class="w-full h-full object-cover"
-            alt="Build image"
-          />
-        </a>
-      </div>
+      <!-- <p class="font-semibold text-xl">A full-stack web developer</p> -->
+      <LetterPullup words="A full-stack web developer" class="font-semibold text-xl" />
+
+      <ClientOnly>
+        <BlurReveal :delay="3" :duration="2">
+          <div class="flex gap-10">
+            <a
+              href="https://www.linkedin.com/in/bryan-agan-42ba0b202/"
+              target="_blank"
+              class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
+            >
+              <NuxtImg
+                :src="isDark ? '/images/linkedin.png' : '/images/linkedin-dark.png'"
+                class="w-full h-full object-cover"
+                alt="Build image"
+              />
+            </a>
+            <a
+              href="https://github.com/xbryan25"
+              target="_blank"
+              class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
+            >
+              <NuxtImg
+                :src="isDark ? '/images/github.png' : '/images/github-dark.png'"
+                class="w-full h-full object-cover"
+                alt="Build image"
+              />
+            </a>
+            <a
+              href="mailto:bryanaganp25@gmail.com"
+              class="rounded-md aspect-square w-12 dark:hover:bg-neutral-700 hover:bg-neutral-200"
+            >
+              <NuxtImg
+                :src="isDark ? '/images/email.png' : '/images/email-dark.png'"
+                class="w-full h-full object-cover"
+                alt="Build image"
+              />
+            </a>
+          </div>
+        </BlurReveal>
+      </ClientOnly>
     </div>
   </div>
 </template>
