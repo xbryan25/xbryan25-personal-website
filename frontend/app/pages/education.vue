209dcac9bc256b866bd0ae4f841d1f4b2091286b
Bryan Agan <bryanagan25@gmail.com>
2026-01-05 19:47:20 +0800

    ui(education-page): add content and animations to education page

--- frontend/app/pages/education.vue
+++ frontend/app/pages/education.vue
@@ -1,3 +1,69 @@
+<script setup lang="ts">
+import type { TimelineItem } from '@nuxt/ui';
+
+const show = ref(false);
+
+const educationItems: TimelineItem[] = [
+  {
+    date: 'Since 2023',
+    title: 'Mindanao State University - Iligan Institute of Technology',
+    icon: 'vaadin:diploma-scroll',
+    description: 'Bachelor of Science in Computer Science',
+  },
+  {
+    date: '2017 to 2023',
+    title: 'Philippine Science High School - Central Mindanao Campus',
+    icon: 'material-symbols:man',
+    description: 'High School (With High Honors)',
+  },
+  {
+    date: '2011 to 2017',
+    title: 'Corpus Christi Parochial School of Iligan',
+    icon: 'bx:child',
+    description: 'Elementary (With Honors)',
+  },
+];
+
+const active = ref(-1);
+let intervalId: number | undefined;
+
+onMounted(() => {
+  // Trigger animation on mount
+  requestAnimationFrame(() => {
+    show.value = true;
+  });
+
+  intervalId = window.setInterval(() => {
+    active.value = (active.value + 1) % (educationItems.length + 1);
+  }, 500);
+});
+
+onUnmounted(() => {
+  if (intervalId) clearInterval(intervalId);
+});
+</script>
+
 <template>
-  <div>Education</div>
+  <div class="flex-1 flex justify-center h-full">
+    <div class="class flex flex-col items-center max-w-200 gap-20">
+      <p
+        class="font-bold text-7xl mt-15 opacity-0 scale-10 transition-all duration-750 ease-out"
+        :class="show ? 'opacity-100 scale-100' : ''"
+      >
+        My Education
+      </p>
+
+      <UTimeline
+        v-model="active"
+        reverse
+        :items="educationItems"
+        size="3xl"
+        :ui="{
+          item: 'even:flex-row-reverse even:-translate-x-[calc(100%-3rem)] even:text-right',
+          container: 'h-50',
+        }"
+        class="translate-x-[calc(50%-1rem)]"
+      />
+    </div>
+  </div>
 </template>
--- frontend/app/pages/experience.vue
+++ frontend/app/pages/experience.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+const show = ref(false);
+
+onMounted(() => {
+  // Trigger animation on mount
+  requestAnimationFrame(() => {
+    show.value = true;
+  });
+});
+</script>
+
 <template>
-  <div>Experience</div>
+  <div class="flex-1 flex justify-center h-full mb-15">
+    <div class="class flex flex-col items-center max-w-200">
+      <p
+        class="font-bold text-7xl mt-15 opacity-0 scale-10 transition-all duration-750 ease-out"
+        :class="show ? 'opacity-100 scale-100' : ''"
+      >
+        My Experience
+      </p>
+    </div>
+  </div>
 </template>
