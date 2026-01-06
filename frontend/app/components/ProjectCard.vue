3ac64e248264d6ad3f841d234a15b004e8808f5e
Bryan Agan <bryanagan25@gmail.com>
2026-01-06 12:41:00 +0800

    feat(projects-page): create ProjectCard and ProjectCards components

---
+++ frontend/app/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col border-2 rounded-xl">
+    <div class="pt-4 px-4 pb-1">
+      <h2 class="font-bold text-2xl">Libris</h2>
+    </div>
+
+    <div class="px-4 py-2 text-justify">
+      <p>
+        A book sharing app that makes user go boom. A book sharing app that makes user go boom.A
+        book sharing app that makes user go boom.A book sharing app that makes user go boom.
+      </p>
+    </div>
+
+    <div class="p-4">
+      <div class="flex flex-wrap gap-2">
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+        <UBadge label="Nuxt" variant="subtle" />
+      </div>
+    </div>
+  </div>
+</template>
