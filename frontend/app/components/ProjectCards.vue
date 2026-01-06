90709917a65a60b413c795b315eb9581edb08a70
Bryan Agan <bryanagan25@gmail.com>
2026-01-06 12:41:00 +0800

    feat(projects-page): create ProjectCard and ProjectCards components

---
+++ frontend/app/components/ProjectCards.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-2 gap-5">
+    <ProjectCard />
+
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+  </div>
+</template>
--- frontend/app/pages/projects.vue
+++ frontend/app/pages/projects.vue
@@ -11,13 +11,15 @@ onMounted(() => {
 
 <template>
   <div class="flex-1 flex justify-center h-full mb-15">
-    <div class="class flex flex-col items-center max-w-200">
+    <div class="class flex flex-col items-center max-w-200 gap-10">
       <p
         class="font-bold text-7xl mt-15 opacity-0 scale-10 transition-all duration-750 ease-out"
         :class="show ? 'opacity-100 scale-100' : ''"
       >
         My Recent Projects
       </p>
+
+      <ProjectCards />
     </div>
   </div>
 </template>
