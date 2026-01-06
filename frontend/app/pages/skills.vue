68db472abcb586e9409f445c23959811fe3bff25
Bryan Agan <bryanagan25@gmail.com>
2026-01-06 16:28:01 +0800

    ui(skills-page): add BoxReveal animation to each skill category and preload images

--- frontend/app/pages/skills.vue
+++ frontend/app/pages/skills.vue
@@ -1,4 +1,36 @@
 <script setup lang="ts">
+definePageMeta({
+  head: {
+    link: [
+      { rel: 'preload', as: 'image', href: '/images/logos/c.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/python.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/javascript.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/typescript.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/java.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/mysql.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/supabase.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/postgresql.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/sqlite.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/tailwindcss.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/qt.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/react.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/vue.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/nuxt.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/flask.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/html-5.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/css-3.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/figma.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/daisyui.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/docker.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/git.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/github.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/cloudinary.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/jira.png' },
+      { rel: 'preload', as: 'image', href: '/images/logos/bitbucket.png' },
+    ],
+  },
+});
+
 const show = ref(false);
 
 const languagesIcons = [
@@ -60,69 +92,77 @@ onMounted(() => {
         <div class="flex flex-col gap-6 items-center">
           <LetterPullup words="Languages" class="font-semibold text-2xl" />
 
-          <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
-            <UTooltip
-              v-for="(languageIcon, index) in languagesIcons"
-              :key="index"
-              :text="languageIcon.name"
-              :delay-duration="0"
-              arrow
-              :ui="{ content: 'text-lg p-4' }"
-            >
-              <NuxtImg :src="languageIcon.icon" class="h-32" />
-            </UTooltip>
-          </UMarquee>
+          <BoxReveal color="#E1251B" :delay="1.0">
+            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+              <UTooltip
+                v-for="(languageIcon, index) in languagesIcons"
+                :key="index"
+                :text="languageIcon.name"
+                :delay-duration="0"
+                arrow
+                :ui="{ content: 'text-lg p-4' }"
+              >
+                <NuxtImg :src="languageIcon.icon" class="h-32" />
+              </UTooltip>
+            </UMarquee>
+          </BoxReveal>
         </div>
 
         <div class="flex flex-col gap-6 items-center">
           <LetterPullup words="Databases" class="font-semibold text-2xl" />
 
-          <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
-            <UTooltip
-              v-for="(databasesIcon, index) in databasesIcons"
-              :key="index"
-              :text="databasesIcon.name"
-              :delay-duration="0"
-              arrow
-              :ui="{ content: 'text-lg p-4' }"
-            >
-              <NuxtImg :src="databasesIcon.icon" class="h-32" />
-            </UTooltip>
-          </UMarquee>
+          <BoxReveal color="#E1251B" :delay="1.0">
+            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+              <UTooltip
+                v-for="(databasesIcon, index) in databasesIcons"
+                :key="index"
+                :text="databasesIcon.name"
+                :delay-duration="0"
+                arrow
+                :ui="{ content: 'text-lg p-4' }"
+              >
+                <NuxtImg :src="databasesIcon.icon" class="h-32" />
+              </UTooltip>
+            </UMarquee>
+          </BoxReveal>
         </div>
 
         <div class="flex flex-col gap-6 items-center">
           <LetterPullup words="Frameworks" class="font-semibold text-2xl" />
 
-          <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
-            <UTooltip
-              v-for="(frameworksIcon, index) in frameworksIcons"
-              :key="index"
-              :text="frameworksIcon.name"
-              :delay-duration="0"
-              arrow
-              :ui="{ content: 'text-lg p-4' }"
-            >
-              <NuxtImg :src="frameworksIcon.icon" class="h-32" />
-            </UTooltip>
-          </UMarquee>
+          <BoxReveal color="#E1251B" :delay="1.0">
+            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+              <UTooltip
+                v-for="(frameworksIcon, index) in frameworksIcons"
+                :key="index"
+                :text="frameworksIcon.name"
+                :delay-duration="0"
+                arrow
+                :ui="{ content: 'text-lg p-4' }"
+              >
+                <NuxtImg :src="frameworksIcon.icon" class="h-32" />
+              </UTooltip>
+            </UMarquee>
+          </BoxReveal>
         </div>
 
         <div class="flex flex-col gap-6 items-center">
           <LetterPullup words="Other Technologies" class="font-semibold text-2xl" />
 
-          <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
-            <UTooltip
-              v-for="(otherTechnologiesIcon, index) in otherTechnologiesIcons"
-              :key="index"
-              :text="otherTechnologiesIcon.name"
-              :delay-duration="0"
-              arrow
-              :ui="{ content: 'text-lg p-4' }"
-            >
-              <NuxtImg :src="otherTechnologiesIcon.icon" class="h-32" />
-            </UTooltip>
-          </UMarquee>
+          <BoxReveal color="#E1251B" :delay="1.0">
+            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+              <UTooltip
+                v-for="(otherTechnologiesIcon, index) in otherTechnologiesIcons"
+                :key="index"
+                :text="otherTechnologiesIcon.name"
+                :delay-duration="0"
+                arrow
+                :ui="{ content: 'text-lg p-4' }"
+              >
+                <NuxtImg :src="otherTechnologiesIcon.icon" class="h-32" />
+              </UTooltip>
+            </UMarquee>
+          </BoxReveal>
         </div>
       </div>
     </div>
