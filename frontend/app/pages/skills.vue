4a90840bfb239c39c57bc5e06f671a733b7532bd
Bryan Agan <bryanagan25@gmail.com>
2026-01-07 14:14:40 +0800

    ui(skills-page): configure skills page to be responsive

--- frontend/app/pages/skills.vue
+++ frontend/app/pages/skills.vue
@@ -84,7 +84,7 @@ onMounted(() => {
 
 <template>
   <div class="flex-1 flex justify-center h-full mb-15">
-    <div class="class flex flex-col items-center max-w-200">
+    <div class="class flex flex-col items-center">
       <p
         class="font-bold text-7xl mt-15 opacity-0 scale-10 transition-all duration-750 ease-out"
         :class="show ? 'opacity-100 scale-100' : ''"
@@ -96,8 +96,8 @@ onMounted(() => {
         <div class="flex flex-col gap-6 items-center w-full">
           <LetterPullup words="Languages" class="font-semibold text-2xl" />
 
-          <BoxReveal color="#E1251B" :delay="1.0" class="w-[60vw]">
-            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+          <BoxReveal color="#E1251B" :delay="1.0" class="max-w-[80vw] md:max-w-[60vw]">
+            <UMarquee pause-on-hover class="[--duration:10s]">
               <UTooltip
                 v-for="(languageIcon, index) in languagesIcons"
                 :key="index"
@@ -115,7 +115,7 @@ onMounted(() => {
         <div class="flex flex-col gap-6 items-center w-full">
           <LetterPullup words="Databases" class="font-semibold text-2xl" />
 
-          <BoxReveal color="#E1251B" :delay="1.0" class="w-[60vw]">
+          <BoxReveal color="#E1251B" :delay="1.0" class="max-w-[80vw] md:max-w-[60vw]">
             <UMarquee pause-on-hover class="[--duration:10s]">
               <UTooltip
                 v-for="(databasesIcon, index) in databasesIcons"
@@ -134,8 +134,8 @@ onMounted(() => {
         <div class="flex flex-col gap-6 items-center w-full">
           <LetterPullup words="Frameworks" class="font-semibold text-2xl" />
 
-          <BoxReveal color="#E1251B" :delay="1.0" class="w-[60vw]">
-            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+          <BoxReveal color="#E1251B" :delay="1.0" class="max-w-[80vw] md:max-w-[60vw]">
+            <UMarquee pause-on-hover class="[--duration:10s]">
               <UTooltip
                 v-for="(frameworksIcon, index) in frameworksIcons"
                 :key="index"
@@ -153,8 +153,8 @@ onMounted(() => {
         <div class="flex flex-col gap-6 items-center w-full">
           <LetterPullup words="Other Technologies" class="font-semibold text-2xl" />
 
-          <BoxReveal color="#E1251B" :delay="1.0" class="w-[60vw]">
-            <UMarquee pause-on-hover class="max-w-[60vw] [--duration:10s]">
+          <BoxReveal color="#E1251B" :delay="1.0" class="max-w-[80vw] md:max-w-[60vw]">
+            <UMarquee pause-on-hover class="[--duration:10s]">
               <UTooltip
                 v-for="(otherTechnologiesIcon, index) in otherTechnologiesIcons"
                 :key="index"
